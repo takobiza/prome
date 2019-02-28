c6d0044686a055344bbc9119c1ad82b67a7e59d1
takobiza <hantoshi@jiboujikinoMacBook-ea.local>
2019-02-28 18:25:24 +0900

    Add  Top Page Design

--- Gemfile
+++ Gemfile
@@ -65,4 +65,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
+gem 'materialize-sass', '~> 1.0.0'
+gem "jquery-rails"
+ gem 'material_icons'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'templates/index'
   devise_for :users
+  resources :templates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'templates#index'
+  root 'home#index'
 end
