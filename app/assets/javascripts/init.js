fc95f2b2624020b27fa015c64e531cae95d196f7
takobiza <hantoshi@jiboujikinoMacBook-ea.local>
2019-03-10 03:14:10 +0900

    Add Top Design

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,14 +5,8 @@ $(document).ready(function(){
   var clipboard = new Clipboard('.clipboard-btn');
   $('input.input_text, textarea#textarea2').characterCounter();
   $('.bxslider').bxSlider({
-    auto: true,
-    speed: 2000,
-    moveSlides: 1,
-    pause: 10000,
-    maxSlides: 2,
     slideWidth: 700,
-    randomStart: false,
-    autoHover: true
+    randomStart: false
   });
   M.updateTextFields();
 });
