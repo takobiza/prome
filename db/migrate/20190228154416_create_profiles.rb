0777fb9ec798b002e49685ff083887b18e47749e
takobiza <hantoshi@jiboujikinoMacBook-ea.local>
2019-03-23 20:21:34 +0900

    Edit DB

--- db/migrate/20190228154416_create_profiles.rb
+++ db/migrate/20190228154416_create_profiles.rb
@@ -1,7 +1,6 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
-      t.references :respondent,           foreign_key: true
       t.references :template,           foreign_key: true
       t.timestamps
     end
