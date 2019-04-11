b34388fb61e834d0ea4abd3e2aebb82e2e304992
takobiza <hantoshi@jiboujikinoMacBook-ea.local>
2019-04-12 01:04:29 +0900

    Fix bxslider

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -3,10 +3,6 @@ $(document).on('turbolinks:load', function() {
 
         var clipboard = new Clipboard('.clipboard-btn');
         $('input.input_text, textarea#textarea2').characterCounter();
-        $('.bxslider').bxSlider({
-            slideWidth: 700,
-            randomStart: false
-        });
         M.updateTextFields();
 
     });
