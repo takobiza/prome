f3f40eab4742fb002f81d4284867f96a792223b7
takobiza <hantoshi@jiboujikinoMacBook-ea.local>
2019-04-16 21:45:15 +0900

    Delete comment

--- app/assets/javascripts/profile-delete.js
+++ app/assets/javascripts/profile-delete.js
@@ -2,7 +2,6 @@ $(document).on('turbolinks:load', function() {
   $(function(){
     $(".is-modal-profile-icon").on('click', function(){
       $(".is-modal__body_gray").text("");
-      // [...Array(5)].map((_, i) => $(".is-modal__body_gray").append(`質問${i +1}: ${$(this).data(`question${i +1}`)} <br />`));
       $(".is-modal__body_gray").append(`${$(this).data(`name`)}さんのプロフィールを削除してもよろしいでしょうか？`);
       var id = $(this).data('id');
       var template_id = $(this).data('template-id');
