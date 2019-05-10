f7bbfc73917ee2ae8e10ff644d9a875e123bc9f8
takobiza <hantoshi@jiboujikinoMacBook-ea.local>
2019-05-10 20:37:12 +0900

    Delete Comment

--- app/assets/javascripts/profile_new_modal.js
+++ app/assets/javascripts/profile_new_modal.js
@@ -1,6 +1,5 @@
 $(document).on('turbolinks:load', function() {	
   if (window.location.href.match(/profiles/) && window.location.href.match(/new/) ){	
-    console.log("ここまで");
     $(".is-modal").fadeIn();
 
     $(".profile-close-btn,.is-modal").unbind().click(function(){
